3bdc585b4d3833ef6387224c36a37435ee0d7282
UzonnaA <70288485+UzonnaA@users.noreply.github.com>
2024-07-01 12:27:05 -0400

    done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...args) {
+    // looping through the args
+    for(i = 0; i < args.length; i++){
+        // looping through the array
+        for(j = 0; j < myArray.length; j++){
+            // if the array finds an arg we need removed
+            if(myArray[j] === args[i]){
+
+                // delete the arg
+                myArray.splice(j,1);
+
+                //This is explained in the doc
+                j -= 1;
+            }
+        }
+    }
+    return myArray;
+    
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
