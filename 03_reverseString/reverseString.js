05bc0f268ff3584531526e232cee131841148c43
UzonnaA <70288485+UzonnaA@users.noreply.github.com>
2024-07-01 10:58:31 -0400

    done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    word_length = word.length;
+    newWord = '';
 
+    for(i = word_length - 1; i >= 0; i--){
+        newWord += word.at(i);
+    }
+
+    return newWord;
 };
 
 // Do not edit below this line
