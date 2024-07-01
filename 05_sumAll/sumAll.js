2a570a204e7e4a8a5bc8600c57b2c1e732973d53
UzonnaA <70288485+UzonnaA@users.noreply.github.com>
2024-07-01 12:37:55 -0400

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(typeof start != "number" || typeof end != "number"){
+        return "ERROR";
+    }
 
+    if(start < 0 || end < 0){
+        return "ERROR";
+    }
+
+    if(start <= end){
+        sum = 0;
+        for(i = start; i <= end; i++){
+            sum += i;
+        }
+    }else{
+        sum = 0;
+        for(i = end; i <= start; i++){
+            sum += i;
+        }
+    }
+    
+    
+
+    return sum;
 };
 
 // Do not edit below this line
