68ce66b5f5b723faa9ab47fcb11668fc313c9743
UzonnaA <70288485+UzonnaA@users.noreply.github.com>
2024-07-05 11:52:36 -0400

    done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(objList) {
+    let oldestAge = 0;
+    let oldestObject;
 
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+
+    for(let i = 0; i < objList.length; i++){
+        if(objList[i].yearOfDeath == undefined){
+            objList[i].yearOfDeath = currentYear;
+        }
+
+        if(objList[i].yearOfDeath - objList[i].yearOfBirth > oldestAge){
+            oldestAge = objList[i].yearOfDeath - objList[i].yearOfBirth;
+            oldestObject = objList[i];
+        }
+
+    }
+
+    return oldestObject;
 };
 
 // Do not edit below this line
