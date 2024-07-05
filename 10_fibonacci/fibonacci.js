1d338d6f1668c0cebc0523bec3f450f843c062db
UzonnaA <70288485+UzonnaA@users.noreply.github.com>
2024-07-05 11:42:57 -0400

    done 2

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let arr = [1,1];
 
+    if(num < 0){
+        return "OOPS";
+    }
+
+    if(num == 0){
+        return 0;
+    }
+
+    if(num > 2){
+        for(let i = 2; i < num; i++){
+            arr.push( (arr[i-1]) + (arr[i-2]) );
+            //console.log(arr)
+        }
+    }
+
+    return arr[num-1];
 };
 
 // Do not edit below this line
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(objList) {
+    let names = objList.map(x => `${x.title}`);
+    return names;
 };
 
 // Do not edit below this line
