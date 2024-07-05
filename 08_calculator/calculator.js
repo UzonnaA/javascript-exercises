909692917232c4d29773febe8c5bfa380642b8bb
UzonnaA <70288485+UzonnaA@users.noreply.github.com>
2024-07-05 11:05:00 -0400

    done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,58 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	mySum = arr.reduce((total, currentNum) => {
+    return total + currentNum;
+  }, 0);
+
+  return mySum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  mySum = arr.reduce((total, currentNum) => {
+    return total * currentNum;
+  }, 1);
 
+  return mySum;
 };
 
-const power = function() {
-	
+const power = function(base, toPower) {
+	let sum = 1;
+
+  if(toPower == 0){
+    return 1;
+  }
+
+  for(let i = 0; i < toPower; i++){
+    sum = sum * base;
+  }
+
+  return sum;
 };
 
-const factorial = function() {
-	
+
+
+const factorial = function(num) {
+	arr = [];
+  if(num == 0){
+    return 1;
+  }
+
+  for(let i = 1; i <= num; i++){
+    arr.push(i);
+  }
+
+  final = arr.reduce((total, currentNum) => {
+    return total * currentNum;
+  }, 1);
+
+  return final;
 };
 
 // Do not edit below this line
