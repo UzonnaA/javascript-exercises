0cefaf1db1925852e1c02300aa4c99526d5f4233
UzonnaA <70288485+UzonnaA@users.noreply.github.com>
2024-07-05 11:21:51 -0400

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (myString) {
+    //let baseCase = true;
+    
+    noPunc = myString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    noSpace = noPunc.replace(/\s/g, '');
+    noCaps = noSpace.toLowerCase();
+    
+    for(let i = 0; i < noCaps.length; i++){
+        let start = i;
+        let end = noCaps.length - (i+1);
 
+        if(start >= end){
+            return true;
+        }
+
+        if(noCaps[start] !== noCaps[end]){
+            return false;
+        }
+
+    }
 };
 
 // Do not edit below this line
